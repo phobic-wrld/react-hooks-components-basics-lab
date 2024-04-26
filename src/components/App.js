9f96f4f15ddd623d9e91dfd5fd3f0c103b262098
joy <blackbearmode1@gmail.com>
2024-04-26 04:07:15 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function NavBar() {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <a href="#home">Home</a> <a href="#about"></a>
     </nav>
   );
 }
@@ -17,14 +17,22 @@ function Home() {
 }
 
 {/* write an <About> component here */}
-
+function About() { 
+  return (
+<div id="about"> 
+<h2>About.</h2> 
+</div> 
+); 
+}
 
 function App() {
   return (
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home />
       {/* add your <About> component here */}
+      <About />
     </div>
   );
 }
